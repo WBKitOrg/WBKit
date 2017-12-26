d6a8620611b1b284e4dd9a3e003cd9fdd9d3b8e6
bennywang <blockwang@sina.cn>
2017-12-26 14:02:09 +0800

    增加NSMutableArray的unretain初始化catogory

---
+++ WBKit/WBCatogory/Foundation+/NSMutableArray+Unretain.h
@@ -0,0 +1,15 @@
+//
+//  NSMutableArray+Unretain.h
+//  WBKit
+//
+//  Created by wangbo on 2017/12/26.
+//  Copyright © 2017年 wangbo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableArray (Unretain)
+
++ (NSMutableArray *)unretainedArray;
+
+@end
