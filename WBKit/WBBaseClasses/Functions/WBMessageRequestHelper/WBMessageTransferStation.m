daba0491c233765f696d304dd7e2c3bff918d9b1
bennywang <blockwang@sina.cn>
2018-01-04 19:54:46 +0800

    规范化messageRequest代码

--- WBKit/WBBaseClasses/Functions/WBMessageRequestHelper/WBMessageTransferStation.m
+++ WBKit/WBBaseClasses/Functions/WBMessageRequestHelper/WBMessageTransferStation.m
@@ -16,17 +16,18 @@
 
 @implementation WBMessageTransferStation
 
-static WBMessageTransferStation * _sharedStation;
 + (WBMessageTransferStation *) sharedStation{
-    if (!_sharedStation) {
+    static dispatch_once_t onceToken;
+    static WBMessageTransferStation * _sharedStation = nil;
+    dispatch_once(&onceToken, ^{
         _sharedStation = [[WBMessageTransferStation alloc] init];
-    }
+    });
     return _sharedStation;
 }
 
--(BOOL)registe:(id)listenner WithIdurl:(NSString *)url{
-    if ([[self.registedRecievers allKeys] containsObject:url]) {
-        if ([[self.registedRecievers valueForKey:url] isEqual:listenner]) {
+-(BOOL)registe:(id)listenner wb_id:(NSString *)identification{
+    if ([[self.registedRecievers allKeys] containsObject:identification]) {
+        if ([[self.registedRecievers valueForKey:identification] isEqual:listenner]) {
             //已经包含url并且有另外的实例使用这个url，注册失败
             return NO;
         }
@@ -35,46 +36,45 @@ static WBMessageTransferStation * _sharedStation;
     }else{
         //未注册过的实例，使用弱引用注册进registedRecievers
         NSValue *value = [NSValue valueWithNonretainedObject:listenner];
-        [self.registedRecievers setObject:value forKey:url];
+        [self.registedRecievers setObject:value forKey:identification];
         return YES;
     }
 }
 
--(BOOL)remove:(id)listenner ForUrl:(NSString *)url{
+-(BOOL)remove:(id)listenner wb_id:(NSString *)identification{
     //注销监听对象
-    if ([self.registedRecievers objectForKey:url]) {
-        [self.registedRecievers removeObjectForKey:url];
+    if ([self.registedRecievers objectForKey:identification]) {
+        [self.registedRecievers removeObjectForKey:identification];
         return YES;
     }
-    NSLog(@"WBMessageTransferStation: Has no listenner with url:%@!",url);
+    NSLog(@"WBMessageTransferStation: Has no listenner with id:%@!",identification);
     return NO;
 }
 
 
--(void)postMessage:(id)message from:(NSString *)fromUrl to:(NSString *)toUrl{
+-(void)postMessage:(id)message from:(NSString *)fromId to:(NSString *)toId{
     
     for (NSString *respUrlStr in _registedRecievers.allKeys) {
         NSURL *respUrl = [NSURL URLWithString:respUrlStr];
-        if ([respUrl.host isEqualToString:[NSURL URLWithString:toUrl].host]) {
-            NSObject<WBMessageRequestCallbackDelegate> *req = [[_registedRecievers objectForKey:fromUrl] nonretainedObjectValue];
+        if ([respUrl.host isEqualToString:[NSURL URLWithString:toId].host]) {
+            NSObject<WBMessageRequestCallbackDelegate> *req = [[_registedRecievers objectForKey:fromId] nonretainedObjectValue];
             NSObject<WBMessageRequestListener> *resp = [[_registedRecievers objectForKey:respUrlStr] nonretainedObjectValue];
             //是一个类,则发送消息
             __strong NSObject<WBMessageRequestCallbackDelegate> *reqer = req;
             __strong NSObject<WBMessageRequestListener> *resper = resp;
             
             if (reqer && resper) {
-                if ([reqer WB_NeedCompish]) {
-                    if ([resper respondsToSelector:@selector(WB_listenRequest:response:)]) {
-                        [resper WB_listenRequest:message response:[reqer WB_NeedCompish]];
+                if ([reqer needComplete]) {
+                    if ([resper respondsToSelector:@selector(listenRequest:response:)]) {
+                        [resper listenRequest:message response:[reqer needComplete]];
                     }else{
                         NSLog(@"WBMessageTransferStation: The calling responser does not listen request!");
                     }
                     //回调后取消callback,确保只回调一次
-                    [reqer setWB_NeedCompish:nil];
-                }
-                else{
-                    if ([resper respondsToSelector:@selector(WB_listenRequest:response:)]) {
-                        [resper WB_listenRequest:message response:^(id responseParams) {
+                    [reqer setNeedComplete:nil];
+                } else {
+                    if ([resper respondsToSelector:@selector(listenRequest:response:)]) {
+                        [resper listenRequest:message response:^(id responseParams) {
                             NSLog(@"WBMessageTransferStation: The requster does not responds callback!");
                         }];
                     }
@@ -82,13 +82,9 @@ static WBMessageTransferStation * _sharedStation;
             }else{
                 NSLog(@"WBMessageTransferStation: An error accoured with requester:%@ , responser:%@",reqer,resper);
             }
-            
         }
     }
-    
 }
-
-
 
 //_registedRecievers懒加载
 -(NSMutableDictionary *)registedRecievers{
