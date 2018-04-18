453df38489e80fa4d88524805fc4084f948ba3f9
UknowHui <wangyahuiuknow@gmail.com>
2018-04-18 18:29:38 +0800

    -m add modelView

---
+++ WBKit/WBBaseClasses/Functions/WBModelView/WBModelView.h
@@ -0,0 +1,17 @@
+//
+//  WBModelView.h
+//  WBKitDemo
+//
+//  Created by Uknow on 2018/4/18.
+//  Copyright © 2018年 wangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WBModelView : UIView
+
++ (WBModelView *)show;
+
+- (void)addWithString:(NSString *)string;
+
+@end
